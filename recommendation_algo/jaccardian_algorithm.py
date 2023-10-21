2e362f2a1582fcdd4e0bbb763a9d31c7ac2b9c52
Luke Wang <lukewang63@gmail.com>
2023-10-21 03:34:13 -0400

    Removed commented out code

--- recommendation_algo/jaccardian_algorithm.py
+++ recommendation_algo/jaccardian_algorithm.py
@@ -68,34 +68,3 @@ def recommend():
 
 if __name__ == "__main__":
     app.run(port=os.getenv("PORT", 8000))
-
-# cards = ["card1", "card2"]  # TODO gen list of cards
-
-# fprint = userToBin("midCareer", True, True, "great", "55000", "200")
-# fobj = open("bin_users.csv")
-
-# nRows = len(fobj.readlines()) - 1
-# fobj.close()
-
-# fobj = open("bin_users.csv")
-# data = csv.DictReader(fobj)
-# sim_cards = np.zeros((len(cards), nRows))
-# user_sims = np.zeros((len(cards), nRows))
-# print(sim_cards, user_sims)
-# for idx, row in enumerate(data):
-#     binary = bitarray(row["fingerprint"])
-#     fprintbinary = bitarray(fprint)
-#     differences = binary ^ fprintbinary
-#     num_differences = differences.count(1)
-#     user_similarity = 1 - (num_differences / 4)
-#     for i in range(len(cards)):
-#         sim_cards[i][idx] = row[cards[i]]
-#         user_sims[i][idx] = user_similarity
-# recommends = {}
-# for j in range(len(cards)):
-#     x = user_sims[j].reshape((-1, 1))
-#     y = sim_cards[j]
-#     model = LinearRegression().fit(x, y)
-#     recommends[cards[j]] = bool((model.intercept_ + model.coef_) > 0.5)
-# print(recommends)
-# fobj.close()
