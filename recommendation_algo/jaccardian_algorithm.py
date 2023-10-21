3c088269ad0849dbcac2c2950235aeb28f4e396b
Luke Wang <lukewang63@gmail.com>
2023-10-21 09:20:29 -0400

    Make jaccardian_algorithm.py look better

--- recommendation_algo/jaccardian_algorithm.py
+++ recommendation_algo/jaccardian_algorithm.py
@@ -26,6 +26,7 @@ def recommend():
         data = request.json
     else:
         return "Not JSON"
+
     occupation = data.get("occupation")
     travelFrequency = data.get("travelFrequency")
     travelInterest = data.get("travelInterest")
@@ -42,30 +43,39 @@ def recommend():
         budget,
     )
 
+    # Get the number of rows in csv file (not using pandas b/c it can't deal w/ binary numbers)
     fobj = open("bin_users.csv")
-
     nRows = len(fobj.readlines()) - 1
     fobj.close()
 
     fobj = open("bin_users.csv")
     data = csv.DictReader(fobj)
+
+    # Create arrays for user similarity and their credit card tastes
     sim_cards = np.zeros((len(cards), nRows))
     user_sims = np.zeros((len(cards), nRows))
-    print(sim_cards, user_sims)
+
     for idx, row in enumerate(data):
         binary = bitarray(row["fingerprint"])
         fprintbinary = bitarray(fprint)
+        # Find the differences between the two user's
         differences = binary ^ fprintbinary
         num_differences = differences.count(1)
+        # Calculate user similarity
         user_similarity = 1 - (num_differences / 4)
+        # Populate arrays
         for i in range(len(cards)):
             sim_cards[i][idx] = row[cards[i]]
             user_sims[i][idx] = user_similarity
+
     recommends = {}
+    # Get all relevant points and apply linear regression
     for j in range(len(cards)):
         x = user_sims[j].reshape((-1, 1))
         y = sim_cards[j]
         model = LinearRegression().fit(x, y)
+        # Recommend card if there's over 50% chance that they will like it based
+        # on similar users
         recommends[cards[j]] = bool((model.intercept_ + model.coef_) > 0.5)
     fobj.close()
     return recommends
